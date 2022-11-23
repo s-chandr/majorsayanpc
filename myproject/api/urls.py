00c7d96b7ee243694a7b93e448660c1bfad3f6ce
s-chandr <chandrasayan15@gmail.com>
2022-11-23 12:24:09 +0530

    complete api

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -3,9 +3,18 @@ from . import views
 from django.urls import include
 urlpatterns = [
     
-    path('',views.getData),
-    path('add/',views.postData),
+    #all gets
+    path('getPayment/',views.getPayment),
+    path('getShop/',views.getShop),
+    path('getReview/',views.getReview),
+    path('getCustomer/',views.getCustomer),
+    path('getOrder/',views.getOrder),
 
-    
+    #all post
+    path('postPayment/',views.postPayment),
+    path('postShop/',views.postShop),
+    path('postReview/',views.postReview),
+    path('postCustomer/',views.postCustomer),
+    path('postOrder/',views.postOrder),
 
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -4,153 +4,98 @@ from rest_framework import permissions
 from main_payments.models import Payment,Shop,Order,Customer,ShopReview
 from .serializer import PaymentSerializer,ShopSerializer,CustomerSerializer,OrderSerializer,ShopReviewSerializer
 
-
+#Payments
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def getData(request):
-    payments = Payment.objects.all()
-    serializer  = PaymentSerializer(payments,many=True) 
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def postData(request): 
-    # print(request.data['payment_id']) #WORKS
-    serializer  = PaymentSerializer(data = request.data)
-    if serializer.is_valid():
-         serializer.save()
-        
-    return Response(serializer.data)
-
-
-
-
-
-
-@api_view(['GET'])
-@permission_classes((permissions.AllowAny,))
-def getData(request):
-    payments = Payment.objects.all()
-    serializer  = PaymentSerializer(payments,many=True) 
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def postData(request): 
-    # print(request.data['payment_id']) #WORKS
-    serializer  = PaymentSerializer(data = request.data)
-    if serializer.is_valid():
-         serializer.save()
-        
-    return Response(serializer.data)
-
-
-
-
-
-
-@api_view(['GET'])
-@permission_classes((permissions.AllowAny,))
-def getData(request):
+def getPayment(request):
     payments = Payment.objects.all()
     serializer  = PaymentSerializer(payments,many=True) 
     return Response(serializer.data)
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
-def postData(request): 
+def postPayment(request): 
     # print(request.data['payment_id']) #WORKS
     serializer  = PaymentSerializer(data = request.data)
     if serializer.is_valid():
          serializer.save()
         
     return Response(serializer.data)
+####################################################################
 
 
 
-
-
+#Reviews
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def getData(request):
-    payments = Payment.objects.all()
-    serializer  = PaymentSerializer(payments,many=True) 
+def getReview(request):
+    review = ShopReview.objects.all()
+    serializer  = ShopReviewSerializer(review,many=True) 
     return Response(serializer.data)
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
-def postData(request): 
-    # print(request.data['payment_id']) #WORKS
-    serializer  = PaymentSerializer(data = request.data)
+def postReview(request): 
+    serializer  = ShopReviewSerializer(data = request.data)
     if serializer.is_valid():
          serializer.save()
         
     return Response(serializer.data)
+#####################################################
 
-
-
-
-
-
+#Shop
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def getData(request):
-    payments = Payment.objects.all()
-    serializer  = PaymentSerializer(payments,many=True) 
+def getShop(request):
+    review = Shop.objects.all()
+    serializer  = ShopSerializer(review,many=True) 
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def postShop(request): 
+    serializer  = ShopSerializer(data = request.data)
+    if serializer.is_valid():
+         serializer.save()
+        
+    return Response(serializer.data)
+################################################################
+
+#Customer 
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def getCustomer(request):
+    review = Customer.objects.all()
+    serializer  = CustomerSerializer(review,many=True) 
     return Response(serializer.data)
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
-def postData(request): 
-    # print(request.data['payment_id']) #WORKS
-    serializer  = PaymentSerializer(data = request.data)
+def postCustomer(request): 
+    serializer  = CustomerSerializer(data = request.data)
     if serializer.is_valid():
          serializer.save()
         
     return Response(serializer.data)
 
 
+################################################################
 
-
-
-
+#order
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def getData(request):
-    payments = Payment.objects.all()
-    serializer  = PaymentSerializer(payments,many=True) 
+def getOrder(request):
+    review = Order.objects.all()
+    serializer  = OrderSerializer(review,many=True) 
     return Response(serializer.data)
+
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
-def postData(request): 
-    # print(request.data['payment_id']) #WORKS
-    serializer  = PaymentSerializer(data = request.data)
+def postOrder(request): 
+    serializer  = OrderSerializer(data = request.data)
     if serializer.is_valid():
          serializer.save()
         
     return Response(serializer.data)
-
-
-
-
-
-@api_view(['GET'])
-@permission_classes((permissions.AllowAny,))
-def getData(request):
-    payments = Payment.objects.all()
-    serializer  = PaymentSerializer(payments,many=True) 
-    return Response(serializer.data)
-
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def postData(request): 
-    # print(request.data['payment_id']) #WORKS
-    serializer  = PaymentSerializer(data = request.data)
-    if serializer.is_valid():
-         serializer.save()
-        
-    return Response(serializer.data)
-
